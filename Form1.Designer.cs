58ed586d9f2905a5ca310cf0c84172a486d85558
sp75 <palamarchuk-sv@mail.ru>
2017-01-03 00:09:02 +0200

    new

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace LENCO_TEKAN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button3 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.ArhivetextBox = new System.Windows.Forms.TextBox();
+            this.OutputFolderText = new System.Windows.Forms.TextBox();
+            this.Input_folder_text = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(377, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(24, 23);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(15, 46);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(68, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Output folder";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(15, 15);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Input folder";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(377, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(24, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(326, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "LENCO TEKAN";
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(377, 74);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(24, 23);
+            this.button4.TabIndex = 26;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Arhive folder";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = global::LENCO_TEKAN.Properties.Settings.Default.autostart;
+            this.checkBox1.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::LENCO_TEKAN.Properties.Settings.Default, "autostart", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.checkBox1.Location = new System.Drawing.Point(20, 135);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(70, 17);
+            this.checkBox1.TabIndex = 27;
+            this.checkBox1.Text = "AutoStart";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Click += new System.EventHandler(this.checkBox1_Click);
+            // 
+            // ArhivetextBox
+            // 
+            this.ArhivetextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::LENCO_TEKAN.Properties.Settings.Default, "Arhivefolder", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.ArhivetextBox.Location = new System.Drawing.Point(94, 76);
+            this.ArhivetextBox.Name = "ArhivetextBox";
+            this.ArhivetextBox.Size = new System.Drawing.Size(277, 20);
+            this.ArhivetextBox.TabIndex = 25;
+            this.ArhivetextBox.Text = global::LENCO_TEKAN.Properties.Settings.Default.Arhivefolder;
+            // 
+            // OutputFolderText
+            // 
+            this.OutputFolderText.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::LENCO_TEKAN.Properties.Settings.Default, "OutputFolder", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.OutputFolderText.Location = new System.Drawing.Point(92, 43);
+            this.OutputFolderText.Name = "OutputFolderText";
+            this.OutputFolderText.Size = new System.Drawing.Size(277, 20);
+            this.OutputFolderText.TabIndex = 21;
+            this.OutputFolderText.Text = global::LENCO_TEKAN.Properties.Settings.Default.OutputFolder;
+            // 
+            // Input_folder_text
+            // 
+            this.Input_folder_text.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::LENCO_TEKAN.Properties.Settings.Default, "InputFolder", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.Input_folder_text.Location = new System.Drawing.Point(92, 12);
+            this.Input_folder_text.Name = "Input_folder_text";
+            this.Input_folder_text.Size = new System.Drawing.Size(277, 20);
+            this.Input_folder_text.TabIndex = 17;
+            this.Input_folder_text.Text = global::LENCO_TEKAN.Properties.Settings.Default.InputFolder;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 170);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.ArhivetextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.OutputFolderText);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Input_folder_text);
+            this.Name = "Form1";
+            this.Text = "LENCO TEKAN v. 1.0.0.1";
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox OutputFolderText;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox Input_folder_text;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox ArhivetextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace LENCO_TEKAN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var fDialog = new FolderBrowserDialog();
+            fDialog.SelectedPath = Input_folder_text.Text;
+            fDialog.ShowDialog();
+            Input_folder_text.Text = fDialog.SelectedPath;
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var fDialog = new FolderBrowserDialog();
+            fDialog.SelectedPath = OutputFolderText.Text;
+            fDialog.ShowDialog();
+            OutputFolderText.Text = fDialog.SelectedPath;
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+/*            FileSystemWatcher watcher = new FileSystemWatcher(Input_folder_text.Text, "*.csv");
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.EnableRaisingEvents = true;*/
+
+            button1.Enabled = false;
+
+            timer1.Start();
+
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType == WatcherChangeTypes.Changed)
+            BuildFile(e.FullPath);
+        }
+
+        const string URL_LENCOLAB = "http://live.lencolink.com/tools/tecan/export_to_csv.aspx?id={0}";//"http://dev.lencolab.com/tools/tecan/export_to_csv.aspx?id={0}";
+        const string LOG_FILE = "log.txt";
+        void BuildFile(string FullPath)
+        {
+            try
+            {
+                FileStream s2 = new FileStream(FullPath, FileMode.Open);
+                if (s2.CanRead && s2.CanWrite )
+                {
+                    s2.Close();
+                    if (File.Exists(FullPath) && FullPath.IndexOf("_reply.csv") == -1)
+                    {
+
+                        string id = "";
+                        string[] csv_f = File.ReadAllLines(FullPath);
+                        WriteLog(String.Format("Read file [{0}]", FullPath));
+                        if (Directory.Exists(ArhivetextBox.Text))
+                        {
+                            File.Delete(Path.Combine(ArhivetextBox.Text, Path.GetFileName(FullPath)));
+                           
+                        }
+
+                        foreach (string item in csv_f)
+                        {
+                            if (String.IsNullOrEmpty(item))
+                            {
+                                continue;
+                            }
+                            id += item/*.ToUpper().Trim('U')*/ + ",";
+                        }
+
+                        string fileout = Path.GetFileNameWithoutExtension(FullPath) + "_reply.csv";  
+
+                        using (WebClient web = new WebClient())
+                        {
+                            web.Proxy = WebRequest.DefaultWebProxy;
+                            web.Credentials = System.Net.CredentialCache.DefaultCredentials;
+                            web.Proxy.Credentials = System.Net.CredentialCache.DefaultCredentials;
+                            web.Encoding = System.Text.Encoding.UTF8;
+
+                            string download_file = Path.Combine(ArhivetextBox.Text, fileout);
+                            string dowload_url = String.Format(URL_LENCOLAB, id.Trim(','));
+
+                            WriteLog(String.Format("Start download file [{0}]", dowload_url));
+                            web.DownloadFile(dowload_url, download_file);
+
+                            if (File.Exists(download_file))
+                            {
+                                WriteLog(String.Format("Download successful [{0}]", download_file));
+
+                                File.Copy(download_file, Path.Combine(OutputFolderText.Text, fileout), true);
+                                WriteLog(String.Format("Copy to output folder [{0}]", Path.Combine(OutputFolderText.Text, fileout)));
+
+                                File.Move(FullPath, Path.Combine(ArhivetextBox.Text, Path.GetFileName(FullPath)));
+
+                                WriteLog(String.Format("File move from [{0}] to {1}", FullPath, Path.Combine(ArhivetextBox.Text, Path.GetFileName(FullPath))));
+                            }
+                            else
+                            {
+                                WriteLog("Error download file:" + dowload_url);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                WriteLog(String.Format( "{0} Exception caught.", e.Message));
+            }
+
+        }
+
+        private void WriteLog(string str)
+        {
+            using (StreamWriter sw = new StreamWriter(Path.Combine(Application.StartupPath, LOG_FILE), true))
+            {
+                sw.WriteLine( "{0} : {1}", DateTime.Now, str );
+            }
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Normal;
+                this.ShowInTaskbar = true;
+                notifyIcon1.Visible = false;
+            } 
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var fDialog = new FolderBrowserDialog();
+            fDialog.SelectedPath = ArhivetextBox.Text;
+            fDialog.ShowDialog();
+            ArhivetextBox.Text = fDialog.SelectedPath;
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) button1.PerformClick();
+        }
+
+        private void checkBox1_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string[] fileEntries = Directory.GetFiles(Input_folder_text.Text);
+            foreach (string fileName in fileEntries)
+                if (Path.GetExtension(fileName).ToLower() == ".csv" && fileName.IndexOf("_reply.csv") == -1) BuildFile(fileName);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var rnd = new Random();
+            String content_body=@"
+$
+<span>{{teg1}}</span>
+<span>{{teg2}}</span>
+<span>{{teg3}}</span>
+,
+<span>{{teg4}}</span>
+<span>{{teg5}}</span>
+<span>{{teg6}}</span>
+,
+<span>{{teg7}}</span>
+<span>{{teg8}}</span>
+<span>{{teg9}}</span>
+</div>";
+                String dd = rnd.Next(897876, 23875775).ToString();
+                  dd =   dd.PadLeft(9);
+                  for (int i = 0; i < 9; i++)
+                  {
+                      content_body = content_body.Replace(String.Format("{{teg{0}}}",i), Convert.ToString(dd[i]));
+                  }
+                 
+        
+
+           //  _page.Replace("<?ext:derogatory_items_removed?>", content_body);
+        }
+    }
+}
